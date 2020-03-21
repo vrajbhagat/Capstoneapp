edee99466716eb6cd1100045dd3b7fc3e5109a7f
Vraj Bhagat <bhagatvr@sheridancollege.ca>
2020-03-20 20:10:12 -0400

    initial commit - linked atlasdb

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://juicepet:jpcap@1234@ds037817.mlab.com:37817/heroku_gjbmmmx4',
+    database: 'mongodb+srv://juicepet:jpcap@juicepet-fcdxs.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'Capstoneapp'
 }
